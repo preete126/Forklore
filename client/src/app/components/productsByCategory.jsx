f7e6bcb5513deec5e77e9ee8981fd4c1735c320b
preete126 <ismailbarakatopeyemi05@gmail.com>
2024-06-22 20:50:12 +0100

    feat: added item display section

--- client/src/app/components/productsByCategory.jsx
+++ client/src/app/components/productsByCategory.jsx
@@ -1,11 +1,13 @@
-import { Alert, AlertDescription, AlertIcon, AlertTitle, Box, CloseButton, Flex, Skeleton, SkeletonText, useDisclosure } from "@chakra-ui/react";
+import { Alert, AlertDescription, AlertIcon, AlertTitle, Box, Button, Card, CardBody, CloseButton, Flex, SimpleGrid, Skeleton, SkeletonText, Text, useDisclosure } from "@chakra-ui/react";
+import Image from "next/image";
 import { useEffect, useRef, useState } from "react";
 import { GetAllProducts } from "../services/product";
+import { FaCartArrowDown } from "react-icons/fa6";
 
 
 
-function ProductByCategory({ categoryProducts }) {
-    const { products, setProducts } = categoryProducts;
+function ProductByCategory() {
+    const [products, setProducts] = useState([]);
     const [isLoading, setIsLoading] = useState(null);
     const isMounted = useRef(true)
     const errors = useRef(false)
@@ -66,12 +68,47 @@ function ProductByCategory({ categoryProducts }) {
                                     top={-1}
                                     onClick={onClose}
                                 />
-                            </Alert> : 
-                                ""
-                            
+                            </Alert> :
+                            ""
+
                         )
                         :
-                        <div>yeahhh</div>
+                        (
+                            <SimpleGrid spacing={5} templateColumns='repeat(auto-fill, minmax(300px, 1fr))'>
+                                {products.map((items, index) =>
+                                    <Card key={index}>
+                                        <CardBody p={4}>
+                                            <Flex justifyContent={"space-between"} pb={5}>
+                                                <div style={{ width: items.old_price !== null ? "80%" : "100%", height: "15em", alignContent: "center", position: "relative" }}>
+                                                    <Image src={items.image_url[0]} layout="fill" objectFit="cover" alt={items.name} style={{ fontSize: "13px", margin: "auto", borderRadius: "7px" }} />
+                                                </div>
+                                                {
+                                                    items.old_price !== null ?
+                                                        <Text width={12} h={6} bg="#EAB308" fontSize={13} color={"white"} textAlign={"center"} alignContent={"center"} borderRadius={12}>{(Math.floor((items.old_price - items.price) / items.old_price) * 100)}%</Text>
+                                                        :
+                                                        ""
+                                                }
+                                            </Flex>
+                                            <Text fontWeight={"600"} fontSize={16} >{items.name}</Text>
+                                            <Text color={"#A3AAB5"} pt="1" pb="5" fontSize={13}>{items.quantity}</Text>
+                                            {
+                                                items.old_price !== null ?
+                                                    <Text textDecoration={"line-through"} fontSize={10} color={"#A3AAB5"}>${items.old_price}.00</Text>
+                                                    :
+                                                    <div style={{fontSize:"10px", visibility:"hidden"}}>yooo</div>
+                                            }
+
+                                            <Flex justifyContent={"space-between"} alignItems={"center"} mt={"auto"}>
+                                                <Text color={"#38A169"} fontWeight={"600"}>${items.price}.00</Text>
+                                                <Button leftIcon={<FaCartArrowDown />} size={"sm"} bg="transparent" color={"#38A169"} borderRadius={20} px="4" border={"1px solid #A3AAB5"}>
+                                                    Cart
+                                                </Button>
+                                            </Flex>
+                                        </CardBody>
+                                    </Card>
+                                )}
+                            </SimpleGrid>
+                        )
                     )
                     :
                     <Flex flexWrap={"wrap"} justifyContent={"space-between"} gap={6}>
--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -1,61 +1,34 @@
 "use client"
-import { Box, Text, FormControl, Input, Button, Icon, Flex } from "@chakra-ui/react";
-import { FiSearch } from "react-icons/fi";
-import { BiSolidShoppingBags } from "react-icons/bi";
-import DefaultLayout from "./components/default";
-import styles from "./page.module.css";
+
 import SideFilter from "./components/sideFilter";
 import ProductByCategory from "./components/productsByCategory";
-import { useState } from "react";
+import { Box, Flex } from "@chakra-ui/react";
+import Index from "./landingpage";
+import DefaultLayout from "./components/default";
+
 
 
 
 export default function Home() {
   // const { isOpen, onOpen, onClose } = useDisclosure()
   // const btnRef = useRef()
-  const [itemByCategory,setItemByCategory] = useState([])
+  // const [itemByCategory,setItemByCategory] = useState([])
 
-  const handleItemDisplayOnClick = (ev)=>{
-    setItemByCategory(ev)
-  }
+  // const handleItemDisplayOnClick = (ev)=>{
+  //   setItemByCategory(ev)
+  // }
 
 
   return (
     <>
       <DefaultLayout>
-        <Box className={`${styles.homebg}`}>
-          <main className={`${styles.wrap}`}>
-            <div>
-              <Text fontWeight="700" fontSize="3em">
-                Groceries Delivered in 90 Minute
-              </Text>
-              <Text fontSize="18">
-                Get your healthy foods & snacks delivered at your doorsteps all day everyday
-              </Text>
-              <FormControl display={"flex"} mt="5">
-                <Input type='search' px="6" h="55" focusBorderColor="transparent" bg="rgba(255,255,255,0.4)" boxShadow={"base"} variant={"unstyled"} borderRightRadius={0} placeholder="Select your products from here" _placeholder={{ fontSize: "small" }} />
-                <Button colorScheme={"green"} width={"170px"} borderStartRadius={0} h="55" leftIcon={<FiSearch />} >Search</Button>
-              </FormControl>
-            </div>
-          </main>
-        </Box>
-
-        <Button display={"flex"} flexDir={"column"} gap={3} colorScheme={"green"} boxSize={"6em"} borderEndRadius={0} pos={"fixed"} right={"0"} bottom={"50%"} transform={"translateY(50%)"} zIndex={9}>
-          <div style={{ display: "flex", gap: "5px", alignItems: "center" }}>
-            <BiSolidShoppingBags size={"1.3rem"} />
-            <div style={{ fontSize: "13px", fontWeight: "400" }}>
-              <span>0</span>
-              <span> Items</span>
-            </div>
-          </div>
-          <Box h="3em" w="5em" borderRadius={3} alignContent={"center"} bg={"white"} fontWeight={"600"} fontSize={"13px"} color={"#38A169"}>$0.00</Box>
-        </Button>
-
+        
+        <Index/>
         <Flex>
-          <SideFilter categoryProducts = {handleItemDisplayOnClick} />
+          <SideFilter/>
 
           <Box w="100%" bg={"#F3F4F6"} p="10">
-            <ProductByCategory categoryProducts={{products:itemByCategory, setProducts: setItemByCategory}} />
+            <ProductByCategory />
           </Box>
         </Flex>
       </DefaultLayout>
